8ab5d3451e316e193dfa1ae65843c0e493aa7f51
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-02-11 20:51:24 +0100

    first commit

---
+++ precache-manifest.35554a1ab1d2b22683f0454621adcc43.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b3eda07ccf95e8f3969f",
+    "url": "/static/css/main.4794be10.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "68c91367791890ef67bf",
+    "url": "/static/js/1.68c91367.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b3eda07ccf95e8f3969f",
+    "url": "/static/js/main.b3eda07c.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "68c91367791890ef67bf",
+    "url": "/static/css/1.0fd113a8.chunk.css"
+  },
+  {
+    "revision": "9ddf7111bfbdeb1e79bb873985feb45b",
+    "url": "/index.html"
+  }
+];
